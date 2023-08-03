b5ef815852c14dedfe861533be1e193074cf77eb
Hamiltondan93 <hamiltondan93@gmail.com>
2023-08-03 22:38:03 +0100

    add joke controller closes #4

--- src/main/java/com/hamiltondan/springjokesapp/services/JokeServiceImpl.java
+++ src/main/java/com/hamiltondan/springjokesapp/services/JokeServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hamiltondan.springjokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
